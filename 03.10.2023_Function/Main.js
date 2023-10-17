a5b3c75f645533505515eb8044d6cfa1dfe402f9
edvingrd <Gra22087@spengergasse.at>
2023-10-17 09:25:57 +0200

    Fertige Hausübung

--- 03.10.2023_Function/Main.js
+++ 03.10.2023_Function/Main.js
@@ -1,19 +1,23 @@
 // Funktion, die unter bestimmten Bedingungen einen Fehler auslöst
 function errorAuswerfen(parameter) {
-    if (parameter <= 3) {
+    if (parameter < 5) {
+      document.getElementById("anzeige1").innerHTML = "Fehler bei diesem Parameter";
+      document.getElementById("anzeige").innerHTML = "";
       throw new Error('Ein Fehler ist aufgetreten!');
     } else {
-      console.log('Kein Fehler bei Parameter:', parameter);
+      document.getElementById("anzeige").innerHTML = 'Kein Fehler bei diesem Parameter';
+      document.getElementById("anzeige1").innerHTML = "";
+      console.log('Kein Fehler bei Parameter');
     }
     // um besser die einzelnen Zeilen zu erkennen
     console.log("------------------------------");
   }
   function startButton()
   {
-    // Schleife, um die Funktion 10-20 Mal aufzurufen
-  const SchleifenAnzahl = Math.floor(Math.random() * 20) ; // Zufällige Anzahl von Aufrufen zwischen 10 und 20
+  // Schleife, um die Funktion 10-20 Mal aufzurufen
+  //const SchleifenAnzahl = Math.floor(Math.random() * 20) ; // Zufällige Anzahl von Aufrufen zwischen 10 und 20
   
-  for (let i = 0; i < SchleifenAnzahl; i++) {
+  for (let i = 0; i < 1; i++) {
     const zufälligerParameter = Math.floor(Math.random() * 10); // Zufälliger Parameter zwischen 0 und 10
     try {
     errorAuswerfen(zufälligerParameter);
